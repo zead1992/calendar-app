5fcca65aa62b3f534cced84cdf83c367a3d4885d
ziad <ziad.alsabouni@outlook.com>
2020-06-08 19:20:40 +0300

    update dependencies: @ngxs/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgxsModule} from "@ngxs/store";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
