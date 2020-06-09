ff34a9514430e9d85c99deff63f6a3a03fbe4c9b
ziad <ziad.alsabouni@outlook.com>
2020-06-09 20:40:08 +0300

    fix month static

--- src/app/utilities/utility.ts
+++ src/app/utilities/utility.ts
@@ -14,7 +14,7 @@ export class Utility {
       '5': 'June',
       '6': 'July',
       '7': 'August',
-      '8:': 'September',
+      '8': 'September',
       '9': 'October',
       '10': 'November',
       '11': 'December'
